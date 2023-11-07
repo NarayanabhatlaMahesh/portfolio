6915c6d305be1f87f1ad67ccb067eeeb38a9c5d0
Mahesh <numamahesh2000@gmail.com>
2023-11-07 11:39:10 +0530

    some changes

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import '@coreui/coreui/dist/css/coreui.min.css';
 import Project from './Project';
 import Navbaar from './Nav';
+import 'animate.css';
+import React from 'react';
 import picture from './picture.jpg';
 import { Avatar } from '@mui/material';
 import Card from '@mui/material/Card';
@@ -9,22 +11,26 @@ import blogpic from './blogpic.png';
 import linear from './linearreg.png';
 import ocrwtp from './ocrwtp.jpg';
 import weatherapp from './weatherapp.png';
+import CardFade from './cardfade';
 
 function Home(){
+    
+
+
     function handleclickLinear(){
         window.location.href = 'https://github.com/NarayanabhatlaMahesh/ipynb_files';
     }
 
-    function handleclickWeather(){
-        window.location.href = 'https://github.com/NarayanabhatlaMahesh/FullStackProjects/weatherProject'
+    function handleclickWeather(){  
+        window.location.href = 'https://github.com/NarayanabhatlaMahesh/weatherProject'
     }
 
     function handleclickecommerce(){
-        window.location.href = 'https://github.com/NarayanabhatlaMahesh/FullStackProjects/ecommerce'
+        window.location.href = 'https://github.com/NarayanabhatlaMahesh/ecommerce'
     }
 
     function handleclickBlog(){
-        window.location.href = 'https://github.com/NarayanabhatlaMahesh/FullStackProjects/bloging'
+        window.location.href = 'https://github.com/NarayanabhatlaMahesh/bloging'
     }
 
     function handleclickOCR(){
@@ -32,8 +38,10 @@ function Home(){
     }
 
     function handleclickchat(){
-        window.location.href = 'https://github.com/NarayanabhatlaMahesh/FullStackProjects/chatproject'
+        window.location.href = 'https://github.com/NarayanabhatlaMahesh/chatproject'
     }
+
+   
 
     const margintop={
         marginTop: '10%',
@@ -63,15 +71,15 @@ function Home(){
     <Navbaar/>
         <Card style={{marginLeft:'15%' , marginRight:'15%', marginTop:'10%', height:window.innerHeight<700?('65%'):('50%'), boxShadow: 'rgba(0, 0, 0, 0.45) 0px 25px 20px -20px'}}>
             <div class="centerleft" style={  (window.innerWidth>425? (window.innerWidth>770? (window.innerWidth>1000?{marginTop:'15%',marginLeft:'10%'}:{marginTop:'40%', marginLeft:'15%'}) :margintop) : martp)}>
-                <h1 style={{letterSpacing:window.innerWidth<768?'2px':'7px', textAlign:'center', maxWidth:'100%', width:'100%'}}> Mahesh Narayanabhatla</h1>
+                <h1 class="animate__animated animate__fadeInLeft" style={{letterSpacing:window.innerWidth<768?'2px':'7px', textAlign:'center', maxWidth:'100%', width:'100%'}}> Mahesh Narayanabhatla</h1>
             </div>
             <div class="centerright" style={window.innerWidth>425?window.innerWidth>770?window.innerWidth>1000?{marginTop:'10%', marginRight:'10%'}:{marginTop:'15%', marginRight:'25%'}:marginto  :right}>
-                <div class="littlemiddle" style={window.innerWidth>1000?{paddingLeft:'18%'}:{paddingLeft:'0%', paddingRight:'20%'}}>
+                <div class="littlemiddle" style={window.innerWidth>1000?{paddingLeft:'5%'}:{paddingLeft:'10%', paddingRight:'20%'}}>
                     <Avatar src={picture} sx={
-                        { width: window.innerWidth>768?150:40, height: window.innerWidth>768?150:40, marginRight:window.innerWidth>765?'10%':window.innerWidth<350?'10%':'50%', marginLeft:window.innerWidth>765?window.innerWidth>900?'20%':'50%':'50%' }
+                        { width: window.innerWidth>768?150:40, height: window.innerWidth>768?150:40, marginRight:window.innerWidth>765?'5%':window.innerWidth<350?'10%':'20%', marginLeft:window.innerWidth>765?window.innerWidth>900?'20%':'50%':'50%' }
                     } />
                 </div>
-                <h4 >
+                <h4>
                     <ul style={{fontSize:'20px', listStyleType:'none', paddingLeft:window.innerWidth<770?'5%':'inherit'}}>
                         <li>Web Developer</li>
                         <li>Software Developer</li>
@@ -89,62 +97,74 @@ function Home(){
         </Card>
     </section>
 
+
+    <section class='skills'>
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
     <section class="projectsSection">
         <h2  style={{fontSize:34, textAlign:'center', paddingBottom:150}}>My Projects</h2>
         
-        <section class="leftsection" >
+        <section class="leftsection"  >
         <div class = "givespace" >
+            <div   onClick={handleclickLinear}>
+            <Project name={"Linear Regression"}   /></div></div>    
+            
             <div onClick={handleclickLinear}>
-            <Project name={"Linear Regression"}   /></div></div>
-
-            <div class="centerise" > <div onClick={handleclickWeather}> <Card  style={{boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px'}}>
-            <img alt="nothing" variant="top" src={weatherapp} height={window.innerWidth<790?'75px':"250px"} />
-                <Project description = {"django and weather api"}   />
-            </Card></div></div>
+            <CardFade  image = {weatherapp} text = 'weather app'  ></CardFade>
+            </div>
 
             <div class = "givespace"  >
                 <div onClick={handleclickecommerce}>
             <Project name={"ecommerce website"}  /></div></div>
 
-            <div class="centerise" > <div onClick={handleclickBlog}> <Card  style={{boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px'}}>
-            <img alt="nothing" variant="top" src={blogpic} height={window.innerWidth<790?'75px':"250px"} />
-            <Project description = {"django bootstrap, include backend"}   />
-            </Card></div></div>
+            <div onClick={handleclickBlog}>
+            <CardFade  image = {blogpic} text =  "django bootstrap, include backend" ></CardFade>
+            </div>
 
             <div class = "givespace" >
                 <div onClick={handleclickchat}>
             <Project name={"chat app"} /></div></div>
 
-            <div class="centerise" > <div onClick={handleclickOCR}> <Card style={{boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px'}}>
-            <img alt="nothing" variant="top" src={ocrwtp} height={window.innerWidth<790?'75px':"250px"} />
-                <Project description={"NLP project, ocr, summarization, translation"} />
-            </Card></div></div>
+            <div onClick={handleclickOCR}>
+            <CardFade  image = {ocrwtp} text =  "NLP project, ocr, summarization, translation"></CardFade>
+            </div>
 
         </section>
 
         <section class="rightsection" >
-            <div class="centerise"> <div onClick={handleclickLinear}> <Card style={{boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px'}}>
-            <img alt="nothing" variant="top" src={linear} height={window.innerWidth<790?'75px':"250px"} />
-            <Project description={"used linear regression on covid deaths prediction"} />
-            </Card></div></div>
+
+            <div onClick={handleclickLinear}>
+            <CardFade  image = {linear} text =  "used linear regression on covid deaths prediction"></CardFade>
+            </div>
 
             <div class = "givespace" >
                 <div onClick={handleclickWeather}>
             <Project name = {"weather app"}  /></div></div>
 
-            <div class="centerise" > <div onClick={handleclickecommerce}><Card style={{boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px'}}>
-                <img alt="nothing" variant="top" src={ecommercepic} height={window.innerWidth<790?'75px':"250px"} />
-            <Project description={"django react"} />
-            </Card></div></div> 
+
+            <div onClick={handleclickecommerce}>
+            <CardFade  image = {ecommercepic} text = "django react"></CardFade>
+            </div>
+
 
             <div class = "givespace" >
                 <div onClick={handleclickBlog} >
             <Project name = {"blog application"}  /></div></div>
 
-            <div class="centerise"  ><div onClick={handleclickchat}><Card style={{boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px'}}>
-                
-                <Project description = {"asynchronous chat rooms, jango, channels"}   />
-            </Card></div></div>
+            <div onClick={handleclickchat}>
+            <CardFade  text = "asynchronous chat rooms, jango, channels"></CardFade>
+            </div>
 
             <div class = "givespace" >
                 <div onClick={handleclickOCR} >
@@ -152,8 +172,15 @@ function Home(){
 
         </section>                
     </section>
+        <section class='footer' style={{height : window.innerHeight/4}} >
+            <p>mae using</p>
+            <div style={{float:'left', Width:window.innerHeight}}>
+                <img height={'50%'} width={'50%'} style={{float:'left',display:'block'}} src="https://w7.pngwing.com/pngs/235/872/png-transparent-react-computer-icons-redux-javascript-others-logo-symmetry-nodejs-thumbnail.png"></img>
+            </div>
+        </section>
+    
     </>
     );
-}
+};
 
 export default Home;
--- src/Project.js
+++ src/Project.js
@@ -1,9 +1,23 @@
-function Project({name, img, description})
+import React from "react";
+
+function Project(props)
 {
+    const [isVisible, setVisible] = React.useState(false);
+    const domRef = React.useRef();
+    React.useEffect(() => {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => setVisible(entry.isIntersecting));
+      });
+      observer.observe(domRef.current);
+    }, []);
+
     return(
         <>
-            <h1>{name}</h1>
-            <p>{description}</p>
+        <div className={`fade-in-section ${isVisible ? 'is-visible' : ''}`} ref={domRef}>        
+            
+            <h1>{props.name}</h1>
+        </div>
+        <p>{props.description}</p>
         </>
     )
 }
