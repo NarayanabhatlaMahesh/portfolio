88a1ce209ab63ac473a614af98820a5207ba1457
MaheshMLOPS <maheshn@atomstate.com>
2024-04-24 22:38:37 +0530

    some commit

--- src/skillcard.js
+++ src/skillcard.js
@@ -4,7 +4,11 @@ function SkillCard(props){
     return(
         <>
         <div>
-            <Card  style={{backgroundColor:'white',boxShadow:'0px 10px 20px 0px rgb(0 0 0 / 5%)', height:'100px', paddingTop:'18%'}}>
+            <Card  style={{height:'100px', paddingTop:'18%',
+        backgroundColor:'hsla(167,0%,100%,1)',
+        backgroundImage:
+        'radial-gradient(at 0% 100%, hsla(239,100%,77%,1) 0px, transparent 50%),radial-gradient(at 80% 100%, hsla(242,100%,70%,1) 0px, transparent 50%'
+        }}>
                 <div style={{ margin:"0 auto"}}>
                     <img src={props.link} style={{maxWidth:"50px",alignItems:"center", margin:"0 auto"}}></img>
                     <p style={{paddingLeft:'25%' , paddingTop:'5px' }}>{props.skill}</p>
