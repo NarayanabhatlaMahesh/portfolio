4880fe5091786758a67bd549edeb5c84ae1a2ddb
Mahesh Narayanabhatla <cb.sc.p2cse24015@cb.students.amrita.edu>
2025-04-29 16:41:13 +0530

    some message

--- src/components/cardfade.js
+++ src/components/cardfade.js
@@ -3,27 +3,31 @@ import Project from "./Project";
 import { Card } from "react-bootstrap";
 
 
-function CardFade(props){
+function CardFade(props) {
 
-    const [isVisible, setVisible] = React.useState(false);
-    const domRef = React.useRef();
-    React.useEffect(() => {
-      const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => setVisible(entry.isIntersecting));
-      });
-      observer.observe(domRef.current);
-    }, []);
+  const [isVisible, setVisible] = React.useState(false);
+  const domRef = React.useRef();
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+  }, []);
 
-    return(
-        <>
-        <div  className={`fade-in-section ${isVisible ? 'is-visible' : ''}`} ref={domRef} >
-            <div class="centerise" > <div> <Card style={{boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px'}}>
-                <img alt="no" variant="top" src={props.image} height={window.innerWidth<790?'75px':"250px"} />
-                <Project description={props.text} />
-            </Card></div></div>
+  return (
+    <>
+      <div className={`fade-in-section ${isVisible ? 'is-visible' : ''}`} ref={domRef} >
+        <div class="centerise" >
+          <div>
+            <Card style={{ boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px', border:'0px' }}>
+              <img alt="no" variant="top" src={props.image} height={window.innerWidth < 790 ? '75px' : "250px"} />
+              <Project description={props.text} />
+            </Card>
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  )
 }
 
 export default CardFade;
--- src/components/skillcard.js
+++ src/components/skillcard.js
@@ -7,7 +7,7 @@ function SkillCard(props) {
                 <Card style={{ backgroundColor: 'white', boxShadow: '0px 10px 20px 0px rgb(0 0 0 / 5%)', height: '100px', paddingTop: '18%' }}>
                     <div className="mx-auto" style={{ margin: "0 auto" }}>
                         <div className="mx-auto flex" style={{display: 'flex'}}>
-                            <img className="mx-auto" alt="none" src={props.link} style={{ maxWidth: "50px" }}></img>
+                            <img className="mx-auto" alt="none" src={props.link} style={{ maxWidth: "50px", maxHeight:'25px' }}></img>
                         </div>
                         <p className="p-2" style={{textAlign:'center'}} >{props.skill}</p>
                     </div>
